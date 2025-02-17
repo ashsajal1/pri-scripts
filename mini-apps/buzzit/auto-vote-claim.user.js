ced8c4d0c894d4fe49d3237e27edb8b6979af89e
Ashsajal <ashsajal1@gmail.com>
2025-02-17 15:46:56 +0600

    edit desc

--- mini-apps/buzzit/auto-vote-claim.user.js
+++ mini-apps/buzzit/auto-vote-claim.user.js
@@ -5,16 +5,30 @@
 // @author       Zaman
 // @match        https://front.buzzitcdn.ru/*
 // @grant        none
-// @icon         https://raw.githubusercontent.com/ilfae/ilfae/main/logo.webp 
+// @icon         https://raw.githubusercontent.com/ilfae/ilfae/main/logo.webp
 // ==/UserScript==
 
 (function () {
-    "use strict";
-  
-    try {
-      
-    } catch (e) {
-      console.error("!Buzzit! error:", e);
+  "use strict";
+
+  try {
+    // Create the controls container if it doesn't exist
+    if (!document.querySelector("#blumfarm-controls")) {
+      const controlsContainer = document.createElement("div");
+      controlsContainer.id = "blumfarm-controls";
+      controlsContainer.style.position = "fixed";
+      controlsContainer.style.top = "0";
+      controlsContainer.style.left = "50%";
+      controlsContainer.style.transform = "translateX(-50%)";
+      controlsContainer.style.zIndex = "9999";
+      controlsContainer.style.backgroundColor = "black";
+      controlsContainer.style.borderRadius = "10px";
+      controlsContainer.style.padding = "5px 10px";
+      controlsContainer.style.color = "white";
+      controlsContainer.textContent = "Auto Voter";
+      document.body.appendChild(controlsContainer);
     }
-  })();
-  
+  } catch (e) {
+    console.error("!Buzzit! error:", e);
+  }
+})();
