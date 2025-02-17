64789912475534d5ca8265906cb6d98b56dd990d
Ashsajal <ashsajal1@gmail.com>
2025-02-17 15:07:34 +0600

    add: skeleton of buzzit

---
+++ mini-apps/buzzit/auto-vote-claim.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         !Buzzit Auto Tasker!
+// @version      1.0
+// @namespace    Zaman
+// @author       Zaman
+// @match        https://telegram.blum.codes/*
+// @grant        none
+// @icon         https://raw.githubusercontent.com/ilfae/ilfae/main/logo.webp
+// @updateURL    
+// ==/UserScript==
+
+(function () {
+    "use strict";
+  
+    try {
+      
+    } catch (e) {
+      console.error("!Buzzit! error:", e);
+    }
+  })();
+  
