e7837e6b46ddbe337725407c6c0c0d69fa3704a2
Ashsajal <ashsajal1@gmail.com>
2025-03-29 14:27:35 +0600

    feat: add Telegram Auto Worker user script

---
+++ mini-apps/tg.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Telegram Auto Worker
+// @description  Auto worker for Telegram bot
+// @version      1.1
+// @namespace    Zaman
+// @author       Zaman
+// @match        https://telegram.blum.codes/*
+// @grant        none
+// @icon         https://raw.githubusercontent.com/ilfae/ilfae/main/logo.webp
+// @updateURL    https://raw.githubusercontent.com/ashsajal1/pri-scripts/refs/heads/master/mini-apps/tg.user.js
+// @downloadURL  https://raw.githubusercontent.com/ashsajal1/pri-scripts/refs/heads/master/mini-apps/tg.user.js
+// ==/UserScript==
