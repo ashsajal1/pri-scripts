ed1d347e7e6fbbb9e6c9247a637e56d934b9b980
Ashsajal <ashsajal1@gmail.com>
2025-04-09 23:22:35 +0600

    chore: bump version to 3.06

--- mini-apps/tg.user.js
+++ mini-apps/tg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Telegram Auto Worker
 // @description  Auto worker for Telegram bot
-// @version      3.05
+// @version      3.06
 // @namespace    Zaman
 // @author       Zaman
 // @match        https://web.telegram.org/*
