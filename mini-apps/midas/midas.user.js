3b1ef454ab8051421b21c30152d93c5488a2697a
Ashsajal <ashsajal1@gmail.com>
2025-04-13 20:36:26 +0600

    feat: create midas script

---
+++ mini-apps/midas/midas.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Midas Auto Worker
+// @description  Auto worker for Telegram Midas bot
+// @version      0.00
+// @namespace    Zaman
+// @author       Zaman
+// @match        https://prod-tg-app.midas.app/*
+// @grant        GM_xmlhttpRequest
+// @connect      localhost
+// @icon         https://raw.githubusercontent.com/ashsajal1/pri-scripts/refs/heads/master/mini-apps/tg.webp
+// @updateURL    https://raw.githubusercontent.com/ashsajal1/pri-scripts/refs/heads/master/mini-apps/midas/midasuser.js
+// @downloadURL  https://raw.githubusercontent.com/ashsajal1/pri-scripts/refs/heads/master/mini-apps/midas/midasuser.js
+// ==/UserScript==
