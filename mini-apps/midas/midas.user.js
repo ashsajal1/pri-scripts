21cf17320cb5eaadea2d043f9fd8f7033efb44d0
Ashsajal <ashsajal1@gmail.com>
2025-04-14 15:41:08 +0600

    chore: bump version to 0.03

--- mini-apps/midas/midas.user.js
+++ mini-apps/midas/midas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Midas Auto Worker
 // @description  Auto worker for Telegram Midas bot
-// @version      0.02
+// @version      0.03
 // @namespace    Zaman
 // @author       Zaman
 // @match        https://prod-tg-app.midas.app/*
