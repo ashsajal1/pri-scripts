531d9188c74c64be5bf4d7166d51750b0f464eda
Ashsajal <ashsajal1@gmail.com>
2025-04-15 17:46:21 +0600

    chore: bump version to 3.03

--- mini-apps/blum/auto-tasker.user.js
+++ mini-apps/blum/auto-tasker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Blum Tasker
-// @version      3.02
+// @version      3.03
 // @namespace    Zaman
 // @author       Zaman
 // @match        https://telegram.blum.codes/*
